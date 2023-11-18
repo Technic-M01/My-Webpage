a3e513a8c6645d734dc723ade789cbecd93a4b8c
TechnicM01 <mauriciohvvilla1@gmail.com>
2023-11-18 15:48:45 -0600

    restructured project, working version of upload script

--- scripts/upload-files.py
+++ scripts/upload-files.py
@@ -2,8 +2,6 @@ import boto3
 import os
 import time
 
-from datetime import datetime, timezone
-
 
 REGION = "us-east-2"
 
@@ -20,34 +18,46 @@ def listBucketObjects():
     for my_bucket_object in my_bucket.objects.all():
         print(my_bucket_object.key)
 
-def isModified():
-    obj = s3.Object(BUCKET_NAME, 'index.html')
+# TODO use both modification times, and file size when checking if file needs upload
+def isModified(file_path: str, debugPrint = False):
 
-    file_modified = int(os.path.getmtime('/home/mauriciov/projects/My-Webpage/index.html'))
+    file_name = file_path.split("/")[-1]
 
-    file_size = os.stat('/home/mauriciov/projects/My-Webpage/index.html')
+    # obj = s3.Object(BUCKET_NAME, 'index.html')
+    obj = s3.Object(BUCKET_NAME, file_name)
+
+    # file_modified = int(os.path.getmtime('/home/mauriciov/projects/My-Webpage/index.html'))
+    file_modified = int(os.path.getmtime(file_path))
+
+    # file_size = os.stat('/home/mauriciov/projects/My-Webpage/index.html')
+    file_size = os.stat(file_path)
 
     # adjusting the 'last_modified' datetime to the local timezone
     lm_to_local = obj.last_modified.astimezone() 
     object_modified = time.mktime(lm_to_local.timetuple())
 
-    print(f"obj modified: {object_modified}\nlocal file modified: {file_modified}\nobj lenght: {obj.content_length}(bytes)\nfile length: {file_size.st_size}(bytes)\nlocal file newer? {object_modified < file_modified}")
+    if debugPrint:
+        print(f"obj modified: {object_modified}\nlocal file modified: {file_modified}\nobj lenght: {obj.content_length}(bytes)\nfile length: {file_size.st_size}(bytes)\nlocal file newer? {object_modified < file_modified}")
 
     return object_modified < file_modified
 
-def uploadFile():
+def uploadFile(key, path):
     # path in s3. ex/ root/pages/resume/picture/newheadshot.jpg
-    key = 'test.txt'
+    # key = 'test.txt'
+
+    body = open(path, 'rb')
 
     s3.Bucket(BUCKET_NAME).put_object(
         Key = key,
-        Body = open('/home/mauriciov/projects/My-Webpage/test.txt', 'rb')
+        Body = body
     )
-
-def downloadFile():
-    s3.Bucket(BUCKET_NAME).download_file('test.txt', 'test2.txt')
+        # Body = open('/home/mauriciov/projects/My-Webpage/test.txt', 'rb')
 
 
+def downloadFile(file_name, download_file_name):
+    s3.Bucket(BUCKET_NAME).download_file(file_name, download_file_name)
+
+#TODO add a user prompt to confirm upload of file 
 def getWebpageFiles():
     cwd = os.getcwd()
 
@@ -55,26 +65,23 @@ def getWebpageFiles():
 
     print(f"Current directory: {cwd}\nParent directory: {parent}")
 
-    files = os.listdir(parent)
+    dir_path = os.path.abspath(os.path.join(parent, "webpage"))
+
+    files = os.listdir(dir_path)
     print(f"files: {files}")
 
-    # file_list = []
+    for f in files:
+        file_path = os.path.abspath(os.path.join(dir_path, f))
+        modified = isModified(file_path, True)
+        print(f"file: {f} modified? {modified}\n")
 
-    # for (root, dirs, file) in os.walk(parent):
-    #     for f in file:
-    #         if '.jpg' in f:
-    #             print(f)
-    #         elif '.css' in f:
-    #             print(f)
-    #         elif '.js' in f:
-    #             print(f)
-    #         elif '.html' in f:
-    #             print(f)
+        if modified:
+            # uploadFile(f, file_path)            
+            print(f"file: {f}\npath: {file_path}")
 
+# print(f"modified: {isModified('/home/mauriciov/projects/My-Webpage/index.html')}")
 
-# modified = isModified()
-
-# print(f"modified: {modified}")
+# getWebpageFiles()
 
 # uploadFile()
-downloadFile()
+# downloadFile("index.html", "new-index.html")
